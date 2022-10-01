b4c2c4961328a1d3c9aaa158a69f1cdb8df6ba70
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-01 23:42:00 +0530

    SquareRoot

--- Add.py
+++ Add.py
@@ -1,4 +1,4 @@
 x = int(input("Enter the first number:"))
 y = int(input("Enter the second number:"))
-sum = x+y
+sum = x + y
 print("The Answer is: ", sum)
---
+++ sqrt.py
@@ -0,0 +1,6 @@
+from math import sqrt
+
+
+x = int(input("Enter the no.:"))
+rt = sqrt(x)
+print("The square root is:", rt)
