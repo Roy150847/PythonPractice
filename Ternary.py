2eff6fdd54158e4ff5d79a46a8188528f33b99d8
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-08 16:00:41 +0530

    Ternary

---
+++ Ternary.py
@@ -0,0 +1,5 @@
+a = 1
+b = 2
+c = 3
+
+print("Hello") if(a>b) else print("ABCD")
--- triangle.py
+++ triangle.py
@@ -4,26 +4,32 @@ a = int(input("Enter the first side:"))
 b = int(input("Enter the second side:"))
 c = int(input("Enter the third side:"))
 
-if a > b and a > c :
+
+
+if a >= b and a >= c :
     if b + c > a:
         print(" Triangle is possible ")
     else :
         print(" Triangle is not possible")
-if b > a and b > c :
+elif b >= a and b >= c :
     if a + c > b:
         print(" Triangle is possible ")
     else :
         print(" Triangle is not possible")
-if c > a and c > b :
+elif c >= a and c >= b :
     if a + b > c :
         print(" Triangle is possible ")
     else :
         print(" Triangle is not possible")
+else :
+    print(" Triangle is not possible")
+
+
 
 
 # The above program in one line
 '''
-if (a > b and a > c and b + c > a) or (b > a and b > c and a + c > b) or (c > a and c > b and a + b > c) :
+if (a >= b and a >= c and b + c > a) or (b >= a and b >= c and a + c > b) or (c >= a and c >= b and a + b > c) :
     print(" Triangle is possible")
 else :
     print("Triangle is not possible")
