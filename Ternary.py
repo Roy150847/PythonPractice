376b6785715d9c1213c661ba4f31380c79c0e1ed
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-09 13:00:21 +0530

    SameLine

--- Ternary.py
+++ Ternary.py
@@ -3,3 +3,5 @@ b = 2
 c = 3
 
 print("Hello") if(a>b) else print("ABCD")
+
+#only for one line operation
--- square.py
+++ square.py
@@ -1,8 +1,9 @@
-length = int(input("Enter the length:"))
-breadth = int(input("Enter the breadth:"))
+
+
+length = int(input(" Enter the length:"))
+breadth = int(input(" Enter the breadth:"))
 
 if length == breadth :
     print(" This is a square ")
 else :
     print(" This is not a square")
-    
