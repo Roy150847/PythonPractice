7848296e869db2d0dee6ff5e70a01ee2788eeb71
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-10 17:12:58 +0530

    Oct10

---
+++ Fibonacci.py
@@ -0,0 +1,8 @@
+n = int(input(" Enter the value of n upto which you want to print the series :"))
+n1, n2 = 0, 1
+print(str(n1) + ", " + str(n2), end = ", ")
+for i in range(n-2) :
+    n3 = n1 + n2
+    print(n3, end=", ")
+    n1 = n2
+    n2 = n3
