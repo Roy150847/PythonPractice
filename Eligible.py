d178a7d9d68d86b973663abfe41ca72004f1fe55
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-10 12:04:52 +0530

    Eligiblity

---
+++ Eligible.py
@@ -0,0 +1,7 @@
+total = int(input(" Enter the total number of days: "))
+present = int(input(" Enter the number of days present: "))
+per = present * 100 / total
+if per >= 75 :
+    print(" The candidate is eligible to take the exams ")
+else :
+    print(" The candidate is not eligible to take the exams ")
