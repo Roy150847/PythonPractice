d663c6d1e25d1ad59f6c616f319553c1c7188aca
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-10 18:35:03 +0530

    Oct10

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+first = int(input(" Enter first number :"))
+operator = input(" Enter the operator(+, -, *, /, %) :")
+second = int(input(" enter the scond number :"))
+if operator == "+" :
+    print(first + second)
+if operator == "-" :
+    print(first - second)
+if operator == "*" :
+    print(first * second)
+if operator == "/" :
+    print(first / second)
+if operator == "%" :
+    print(first % second)
