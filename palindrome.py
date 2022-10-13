62bf629853293d08d91bf053faaf3ca52841fa88
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-13 18:09:58 +0530

    Oct13

--- palindrome.py
+++ palindrome.py
@@ -1,22 +1,24 @@
 
 
 str = str(input(" Enter the string to be checked :"))
+
 '''
-for i in range(0, int(len(str)/2)):
-        if str[i] != str[len(str)-i-1]:
-            print(" It's not a palindrome ")
-        else :
-            print(" It's a palindrome ")
- '''   
- 
 def Palindrome(str):
     for i in range(0, int(len(str)/2)):
         if str[i] != str[len(str)-i-1]:
             return False
     return True
 
-ans=Palindrome(str)
+ans = Palindrome(str)
 if ans == False :
     print("It's not a palindrome")
 else :
     print("It's a palindrome")
+'''
+
+
+rev = str[::-1]
+if(rev == str):
+    print("It's a palindrome")
+else :
+    print("It's not a palindrome")
---
+++ samyak.py
@@ -0,0 +1,8 @@
+
+i=1
+j=1
+print(i)
+print(j)
+for x in range(0,20):
+    print(i+j)
+    i,j=j,i+j
