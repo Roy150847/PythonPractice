af6b74c496c1e07fbbe3fe036241723b76e8f41b
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-13 17:36:02 +0530

    Oct13

---
+++ 123.py
@@ -0,0 +1,8 @@
+p=int(input("p:"))
+q=int(input("q:"))
+a = hex(p)
+x = str(p)
+y = str(q)
+
+b ="("+x+"+"+y+"j)"
+print(b)
--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,22 @@
 
 
 str = str(input(" Enter the string to be checked :"))
-
+'''
 for i in range(0, int(len(str)/2)):
         if str[i] != str[len(str)-i-1]:
             print(" It's not a palindrome ")
         else :
             print(" It's a palindrome ")
-    
+ '''   
  
+def Palindrome(str):
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
+
+ans=Palindrome(str)
+if ans == False :
+    print("It's not a palindrome")
+else :
+    print("It's a palindrome")
