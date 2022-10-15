eda91feff041eb95cf309d5c18e25c565b439e64
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-15 15:40:25 +0530

    Oct15

--- Array.py
+++ Array.py
@@ -1,11 +1,13 @@
-'''
-array1 = ["V", "I", "S", "W", "A", "J", "E", "E", "T"]
+
+array1 = ["V", "I", "S", "W", "A", "J", "E", "E", "T"]   
+
+# Array stores same data type whie list can store different data types
+
 
 print(len(array1))     # 9 
 print(array1[0])       # V
 print(array1[8])       # T
 print(array1[4])       # A
-print(array1[0:4])
-'''
-var = "Hello World"
-print(len(var))
+print(array1[0:4])     # ['V','I','S','W']
+print(array1[-1])      # T
+print(array1[-1:-6])
