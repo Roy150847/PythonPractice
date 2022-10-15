9e548fb5ee04552d8485a066a0b31f5ddedf7683
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-15 13:56:17 +0530

    Oct15

---
+++ Array.py
@@ -0,0 +1,11 @@
+'''
+array1 = ["V", "I", "S", "W", "A", "J", "E", "E", "T"]
+
+print(len(array1))     # 9 
+print(array1[0])       # V
+print(array1[8])       # T
+print(array1[4])       # A
+print(array1[0:4])
+'''
+var = "Hello World"
+print(len(var))
---
+++ Random.py
@@ -0,0 +1,3 @@
+import random
+
+print(random.randrange(0,100))  # will print any random no. from 0 to 100 including 0 and excluding 100
