8e8a3742d0bbda247ddc0998dc52cd6fe9cbee34
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-15 09:34:41 +0530

    Oct15

---
+++ Digit.py
@@ -0,0 +1,10 @@
+a = int(input(" Enter the number: "))
+if a > 99 and a < 1000 :
+    print(" Number on unit's place is :",end=" ")
+    print(a%10)
+    print(" Number on ten's place is :",end=" ")
+    print(int((a%100-a%10)/10))
+    print(" Number on hundred's place is :",end=" ")
+    print(int((a-a%100)/100))
+else :
+    print(" Number is not three digit ")
