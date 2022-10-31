7a298214b1354279c9d5c9fda9b95c6650cbaa73
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-10-31 23:35:19 +0530

    Oct31

---
+++ Operations.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the number(n):"))
+n1 = int(input("Enter second number(n1):"))
+
+# HEXADECIMAL VALUE
+hex = hex(n)
+hex1 = hex(n1)
+print("Hexadecimal value of n =",hex)
+print("Hexadecimal value of n1 =",hex1)
+
+# UNICODE NUMBERS
+#unicode = ord(n)
+#unicode1 = ord(n1)
+#print("Unicode of n =",unicode)
+#print("Unicode of n1 =",unicode1)
