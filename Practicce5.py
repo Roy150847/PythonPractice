3b612aed7aea4a7d57fffba10478b303d0f5dda0
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-11-05 03:47:53 +0530

    Nov5

---
+++ Practicce5.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the number: "))
+fact = 1
+while n >= 1 :
+    fact = fact * n 
+    n = n - 1
+print("Factorial of the given number: ",fact)
---
+++ Practice4.py
@@ -0,0 +1,4 @@
+list1=["Phil","Oz","Seuss","Dre"]
+for i in list1:
+    list1=["Dr. "+i]
+print(list1)
