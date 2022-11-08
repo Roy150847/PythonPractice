c31c7fdbeea654580e7f40f9e3f2cb3587f5245f
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-11-08 23:58:24 +0530

    Nov8

---
+++ SumWhile.py
@@ -0,0 +1,10 @@
+n1 = int(input("n1:"))
+n2 = int(input("n2:"))
+n3 = int(input("n3:"))
+list = n1, n2,n3
+largest = 0
+for i in list :
+    if i > largest :
+        largest = i
+
+print("Largest=",largest)
