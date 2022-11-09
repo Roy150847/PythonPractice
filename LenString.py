2a7cf7debddf27a96a36fce06a323ab4163a3197
DESKTOP-38E4BRJ\roys9 <roys930430@gmail.com>
2022-11-09 20:31:09 +0530

    Nov9

---
+++ LenString.py
@@ -0,0 +1,7 @@
+# A program to find length of a string without using built-in function
+
+str = input("Enter the string: ")
+count = 0
+for i in str:
+    count = count + 1
+print("Length of a string: ",count)
